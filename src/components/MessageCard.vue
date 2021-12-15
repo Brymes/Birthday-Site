830891a873bba0abf278ad6b5fd9f683563091cc
Timothy Odebunmi <dev.brymes@gmail.com>
2021-12-15 20:38:51 +0100

    PROJECT INIT

---
+++ src/components/MessageCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card lg:card-side bordered">
+  <div class="card-body">
+    <h2 class="card-title">{{ data.title }}</h2> 
+    <p>{{ data.message }}</p> 
+    <div class="card-actions">
+      <button class="btn btn-primary">Get Started</button> 
+      <button class="btn btn-ghost">More info</button>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+const Message = {
+  name: "Message",
+  props: {
+    data: {
+      required: true,
+      type: Object,
+    }
+  },
+
+  /* methods: {
+    handleShow() {
+      this.show = false;
+      if (this.nxtRoute) {
+        this.$router.push(`/${this.nxtRoute}`);
+      } else {
+        window.location.reload();
+      }
+    },
+  }, */
+};
+export default Message;
+</script>
---
+++ src/components/MessageList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="shadow-xl"  v-for="mess in allMessages"
+      :key="mess.name">
+
+  </div>
+</template>
+
+<script>
+export default {
+// Fetch all messages
+}
+</script>
+
+<style>
+
+</style>
