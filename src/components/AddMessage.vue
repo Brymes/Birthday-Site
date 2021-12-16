58edba5cf6362f9186166ec2c489021a84c2d59b
Timothy Odebunmi <dev.brymes@gmail.com>
2021-12-16 04:35:02 +0100

    Add Message Card

---
+++ src/components/AddMessage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="form-control">
+    <label class="label">
+      <span class="label-text">Enter Message</span>
+    </label>
+    <textarea
+      class="textarea h-24 textarea-bordered textarea-accent"
+      placeholder="Birthday Message"
+    />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
