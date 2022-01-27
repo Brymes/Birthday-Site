5940c08c88656f8759e6d5b442fdfa448a763d52
Timothy Odebunmi <dev.brymes@gmail.com>
2022-01-27 19:57:05 +0100

    Submit Message

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hero min-h-screen">
+    <div class="flex-col hero-content lg:flex-row-reverse">
+      <Carousel />
+      <div>
+        <h1 class="mb-4 text-3xl font-bold">
+          Drop A Message
+        </h1>
+
+        <textarea
+          class="mb-5 textarea h-20 w-max text-white"
+          placeholder="Bio"
+        />
+
+        <button class="btn btn-primary">
+          Get Started
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Carousel from "./Carousel.vue";
+export default { name: "Home", components: { Carousel } };
+</script>
