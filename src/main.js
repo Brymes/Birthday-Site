a6d098ef9772e50f9637fbbe9634157a4fe4f8c5
Timothy Odebunmi <dev.brymes@gmail.com>
2022-01-27 19:52:12 +0100

    adding Tailwind

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import "./index.css";
+
+import "@coreui/coreui/dist/css/coreui.min.css";
+
+createApp(App).mount("#app");
