d1915c0f9f083198bca915232199457acca372f6
Timothy Odebunmi <dev.brymes@gmail.com>
2022-01-27 19:51:59 +0100

    Message List component Scaffold

--- src/components/MessageList.vue
+++ src/components/MessageList.vue
@@ -1,16 +1,23 @@
 <template>
-  <div class="shadow-xl"  v-for="mess in allMessages"
-      :key="mess.name">
-
+  <div
+    v-for="mess in allMessages"
+    :key="mess.name"
+  >
+    <div class="card shadow-lg">
+      <div class="card-body">
+        <h2 class="card-title">
+          {{ mess.name }}
+        </h2>
+        <p>{{ mess.message }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-// Fetch all messages
-}
+  // Fetch all messages
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
