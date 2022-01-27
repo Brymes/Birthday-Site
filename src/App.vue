c83905a91a0d8ba851fc953ebe464166d26644ba
Timothy Odebunmi <dev.brymes@gmail.com>
2022-01-27 19:57:17 +0100

    Random stuffs

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import Carousel from "./components/Carousel.vue";
+
+import Home from "./components/Home.vue";
 </script>
 
 <template>
-  <Carousel />
+  <Home />
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
