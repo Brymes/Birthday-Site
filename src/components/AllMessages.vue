719b591947c5a722ec9cbded892efb5b6f7a7e27
Timothy Odebunmi <dev.brymes@gmail.com>
2022-01-28 23:23:32 +0100

    Switch from all messages to addMessage

--- src/components/AllMessages.vue
+++ src/components/AllMessages.vue
@@ -1,22 +1,64 @@
 <template>
   <div class="w-full mx-4">
-    <p class="text-3xl font-bold uppercase">
-      Messages
+    <p class="text-2xl font-bold uppercase">
+      Birthday Wishes
     </p>
-    
-    <div class="card shadow-sm bg-primary text-primary-content my-8">
-      <div class="card-body">
-        <p class="text-lg">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos fugiat repellat eos mollitia quibusdam, sequi quaerat vel qui rem et provident voluptas quisquam odit obcaecati assumenda repellendus neque possimus soluta a, corporis harum quas amet sit. Necessitatibus incidunt laboriosam voluptatibus?
-        </p> 
-        <p class="card-title font-bold text-2xl text-right capitalize">
-          oladunni odetunde
-        </p> 
+
+    <span
+      class="bg-green-400 py-2"
+      @click="addMessage"
+    >
+      <button class="btn btn-xs md:btn-sm lg:btn-md xl:btn-lg">
+        {{ buttonText }}
+      </button>
+    </span>
+    <div>
+      <AddMessage v-if="messageInput" />
+    </div>
+    <div v-if="!messageInput">
+      <div class="card shadow-sm bg-yellow-400 text-red-900 my-8Î">
+        <div class="card-body">
+          <p class="text-lg">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos
+            fugiat repellat eos mollitia quibusdam, sequi quaerat vel qui rem et
+            provident voluptas quisquam odit obcaecati assumenda repellendus
+            neque possimus soluta a, corporis harum quas amet sit.
+            Necessitatibus incidunt laboriosam voluptatibus?
+          </p>
+          <p class="card-title font-bold text-2xl text-right capitalize">
+            oladunni odetunde
+          </p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import AddMessage from "./AddMessage.vue";
+
+export default {
+  components: { AddMessage },
+  props: {
+    messages: {
+      required: true,
+      type: Array,
+    },
+  },
+
+  data() {
+    return {
+      messageInput: false,
+      buttonText: "Drop a Message",
+    };
+  },
+  methods: {
+    addMessage() {
+      this.messageInput = this.messageInput ? false : true;
+      this.buttonText = this.messageInput
+        ? "See all Messages"
+        : "Drop a Message";
+    },
+  },
+};
 </script>
