300d110f19c9ee92f116427ba822af1880813bdd
Timothy Odebunmi <dev.brymes@gmail.com>
2022-01-28 23:21:13 +0100

    Add Message WOrks Fine

--- src/components/AddMessage.vue
+++ src/components/AddMessage.vue
@@ -1,20 +1,99 @@
 <template>
-  <div class="w-full mx-12">
-    <label class="label">
-      <span class="text-3xl font-bold uppercase">Enter Message</span>
-    </label>
-    <textarea
-      cols="30" 
-      rows="3" 
-      class="textarea w-full p-6 textarea-bordered my-4"
-      placeholder="Birthday Message"
-    />
-    <button class="btn btn-primary btn-block">
-      Submit
-    </button>
+  <div class="card mt-8 mb-6">
+    <Form
+      :validation-schema="yupSchema"
+      @submit="submitMessage"
+    >
+      <div>
+        <label
+          class="
+            block
+            uppercase
+            text-slate
+            label-text
+            text-black
+            font-bold
+            py-2
+            ml-4
+          "
+        >
+          Full Name
+        </label>
+        <Field
+          name="name"
+          type="text"
+          class="
+            px-3
+            py-3
+            placeholder-slate
+            text-slate
+            bg-gray-300
+            text-sm
+            w-60
+            ml-4
+          "
+          placeholder="John"
+        />
+        <ErrorMessage
+          name="name"
+          class="error-feedback text-red-600"
+        />
+      </div>
+      <label class="label">
+        <span
+          class="
+            block
+            uppercase
+            text-slate
+            label-text
+            text-black
+            font-bold
+            mb-2
+            ml-4
+          "
+        >Enter Message</span>
+      </label>
+      <Field
+        class="textarea px-4 h-40 w-60 bg-gray-300"
+        placeholder="Enter Your message"
+        name="message"
+        type="text"
+      />
+      <ErrorMessage
+        name="message"
+        class="error-feedback text-red-600"
+      />
+      <button class="mt-4 maxw-60 btn btn-secondary btn-block">
+        Submit
+      </button>
+    </Form>
   </div>
 </template>
 
 <script>
-export default {};
+import { Form, Field, ErrorMessage } from "vee-validate";
+import { messageSchema } from "./formValidators.js";
+
+import ApiService from "../services/api.service";
+
+export default {
+  components: { Form, Field, ErrorMessage },
+  data() {
+    return {
+      yupSchema: messageSchema,
+    };
+  },
+
+  methods: {
+    async submitMessage(payload) {
+      console.error(payload);
+      try {
+        await ApiService.addWish(payload);
+        window.location.reload();
+      } catch (error) {
+        window.location.reload();
+      }
+    },
+  },
+};
 </script>
